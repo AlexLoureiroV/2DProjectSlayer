2a59c0c2ff3de07744b62ed86b8a6e4ce07d358a
ALEX\alexl <a.l.e.x.lou88@gmail.com>
2026-05-07 05:04:09 +0200

    Mejora de barra de vida Nuevas animaciones:   - Caída Nuevo Texto de altura Video Intro (Scene 01)

---
+++ Assets/Scripts/IntroController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class IntroController : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer _videoPlayer;
+    [SerializeField] private TextMeshProUGUI _pressStartText;
+    [SerializeField] private float _skipAfterSeconds = 2f; // puedes saltar a partir de este momento
+
+    private bool _videoFinished = false;
+
+    void Start()
+    {
+        _pressStartText.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Detectar fin de vídeo por tiempo (más fiable que el evento)
+        if (!_videoFinished && _videoPlayer.isPrepared)
+        {
+            double duration = _videoPlayer.length;
+            if (duration > 0 && _videoPlayer.time >= duration - 0.1)
+            {
+                _videoFinished = true;
+            }
+        }
+
+        // Permitir saltar a partir de _skipAfterSeconds
+        bool canSkip = _videoPlayer.time >= _skipAfterSeconds;
+
+        if (canSkip && (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Escape)))
+        {
+            SceneManager.LoadScene("InGame");
+        }
+
+        // Mostrar texto "Press Start" cuando el vídeo ha terminado
+        if (_videoFinished && !_pressStartText.gameObject.activeSelf)
+        {
+            _pressStartText.gameObject.SetActive(true);
+        }
+    }
+}
